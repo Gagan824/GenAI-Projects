cc79df2693c824b08b335dfcaa066d36752f5656
Gagan Gupta <guptagagan824@gmail.com>
2024-07-27 00:28:34 +0530

    llama 3.1 model

---
+++ llama_3_1/.ipynb_checkpoints/llama_app-checkpoint.py
@@ -0,0 +1,23 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_ollama.llms import OllamaLLM
+
+import streamlit as st
+
+
+st.title("Ask me anything")
+
+template = """Question: {question}
+
+Answer: """
+
+prompt = ChatPromptTemplate.from_template(template)
+
+model = OllamaLLM(model="llama3.1")
+
+chain = prompt | model
+
+
+question = st.chat_input("How may I help you?")
+if question: 
+    st.write(question)
+    st.write(chain.invoke({"question": question}))
